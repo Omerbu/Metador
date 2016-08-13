1ee70d1be253572580ce6b2783c19750d46e4c12
Omerbu <omer.buchnik@gmail.com>
2016-08-13 18:51:43 +0300

    Some Changes to tags retriever
    Header.

--- meta_tags_retriever.py
+++ meta_tags_retriever.py
@@ -5,66 +5,70 @@ import pygn
 import urllib
 import discogs_client
 from bs4 import BeautifulSoup
-from meta_utils import func_decorator
 import pylast
+from meta_utils import func_decorator
 
-pygn_userID = "27680793551513575-12D4F149E8C4D17B6E76872200F49455"
+# GraceNote API keys:
+pygn_userID = "279882536723493437-7A075FF81D3300554B7C166A0961BE5A"
 pygn_clientID = '1735505540-C6C936F2054DA76A0D98E82163189CF0'
-# userID = pygn.register(clientID)        # register time is about 2 sec.
+# userID = pygn.register(pygn_clientID)        # register time is about 2 sec.
 
+# DisCogs API keys:
 discogs_access_token = "LifLerwCJirFtBnEFUDXnhXOWPfZiuBpKutzuLVW"
 discogs_user_agent = "Metador_Application_AutoTagger"
 
+# LastFM API keys:
 lastfmKey = "78b2431b39da9c451b8c000b02c687d7"
 lastfmSecret = "6b8c8647c71244ff4faca5aa5efc73ac"
 
 
-@func_decorator
-def main(artist_name, track_name):
+def main_search(artist_name, track_name):
     """Receive artist + track name and return music-tags from GraceNote API.
         If Pygn results are incomplete, runs a secondary search algorithm that searches
         discogs DB and LastFM DB (for now)
     """
-    tags_results = pygn.search(pygn_clientID, pygn_userID,
+    pygn_results = pygn.search(pygn_clientID, pygn_userID,
                                artist=artist_name, track=track_name)
-    tags_dictionary = dict()
-    tags_dictionary['album'] = tags_results["album_title"]
-    tags_dictionary['year'] = tags_results["album_year"]
-    tags_dictionary['track_number'] = tags_results["track_number"]
-    tags_dictionary['track_title'] = track_name
-    tags_dictionary["artist"] = artist_name
-    tags_dictionary["album_artist"] = tags_results["album_artist_name"]
-    tags_dictionary["genre"] = tags_results['genre']['3']['TEXT']
-    image_url = tags_results["album_art_url"]
-    if image_url:
-        image_binary_string = urllib.urlopen(image_url).read()
+    tags_dict = dict()
+    tags_dict['Album'] = pygn_results["album_title"]
+    tags_dict['Date'] = pygn_results["album_year"]
+    tags_dict['Tracknumber'] = pygn_results["track_number"]
+    tags_dict['Title'] = track_name
+    tags_dict["Artist"] = artist_name
+    tags_dict["Albumartist"] = pygn_results["album_artist_name"]
+    tags_dict["Genre"] = pygn_results['genre']['3']['TEXT']
+    album_art_url = pygn_results["album_art_url"]
+    if album_art_url:
+        image_binary_string = urllib.urlopen(album_art_url).read()
     else:
-        print "No Pygn album art"
-        image_binary_string = None
-    validate_tags(tags_dictionary, artist_name, track_name)
-    debug_print(tags_dictionary)
-    return tags_dictionary, image_binary_string
+        print "No Pygn album art"   # Dev Purposes
+        image_binary_string = lastfm_album_art(
+            tags_dict["Artist"], tags_dict["Album"])
+    validate_tags(tags_dict, artist_name, track_name)
+    debug_print(tags_dict)
+    return tags_dict, image_binary_string
 
 
-def validate_tags(tags_dictionary, artist_name, track_name):
+def validate_tags(tags_dict, artist_name, track_name):
     """Checks if Pygn search has returned all the required tags, if not
     will run secondary search algorithm.
     """
-    for key in tags_dictionary:
-        if not tags_dictionary[key]:
-            discogs_search(tags_dictionary, tags_dictionary["album"], artist_name, )
+    for key in tags_dict:
+        if not tags_dict[key]:
+            discogs_search(tags_dict, tags_dict["Album"],
+                           artist_name, track_name)
+            return
 
 
-@func_decorator
-def discogs_search(tags_dictionary, album_string, artist_name, track_name):
+def discogs_search(tags_dict, album_string, artist_name, track_name):
     """
     Working but needed to add an option to search without artist.
     :return:
     """
-    print "seconary search activated"
+    print "Secondary search activated"
     client = discogs_client.Client(user_agent=discogs_user_agent,
                                    user_token=discogs_access_token)
-    results = client.search(title=artist_name,
+    results = client.search(title=album_string,
                             artist=artist_name,
                             type="master")
     master_id = results[0].id
@@ -72,35 +76,34 @@ def discogs_search(tags_dictionary, album_string, artist_name, track_name):
     album_result = master_result.main_release
     for track in album_result.tracklist:
         if track.title == track_name:
-            tags_dictionary["track_number"] = track.position
+            tags_dict["Tracknumber"] = track.position
             break
-    tags_dictionary["genres"] = album_result.genres[0]
-    tags_dictionary["year"] = album_result.year
+    tags_dict["Genre"] = album_result.genres[0]
+    tags_dict["Date"] = album_result.year
 
 
-def lastfm_search(artist_name, track_name):
-    """
-    Experimental Phase!!!
-    :param artist_name:
-    :param track_name:
-    :return:
-    """
+def lastfm_album_art(artist_name, album_name):
+    """Returns Album Cover from lastfm API."""
     lastfm_client = pylast.LastFMNetwork(lastfmKey, lastfmSecret)
-    result = lastfm_client.get_track(artist_name, track_name)
-    album = result.get_album()
-    album_url = album.get_url()
-    print album.get_cover_image()
-    url = urllib.urlopen(album_url.encode("UTF-8"))
-    album_soup = BeautifulSoup(url.read(), "html.parser")
+    album = lastfm_client.get_album(artist_name, album_name)
+    cover_image_url = album.get_cover_image()
+    image_binary_string = urllib.urlopen(cover_image_url).read()
+    return image_binary_string
 
 
 def brainz_album_art(album_name, artist_name):
-    """Crawl the musicbrainz website in search for album art cover."""
+    """Crawl the musicbrainz website in search for album art cover.
+
+    Since Web Crawling takes time, this should be used only after all APIs
+    failed to find album art.
+
+    """
     parsed_album = album_name.replace(" ", "+")
     parsed_artist = artist_name.replace(" ", "+")
     search_url = \
-        u"https://musicbrainz.org/taglookup?tag-lookup.artist={0}&tag-lookup.release={1}"\
-            .format(parsed_artist, parsed_album)
+        u"https://musicbrainz.org/" \
+        u"taglookup?tag-lookup.artist={0}&tag-lookup.release={1}".format(
+            parsed_artist, parsed_album)
     url = urllib.urlopen(search_url.encode("UTF-8"))
     html_string = url.read()
     soup = BeautifulSoup(html_string, "html.parser")
@@ -118,17 +121,16 @@ def brainz_album_art(album_name, artist_name):
     return image_binary_string
 
 
-def debug_print(tags_dictionary):
+def debug_print(tags_dict):
     """
     Printing For debuging purposes, won't be in final version.
-    :param tags_dictionary:
+    :param tags_dict:
     :return:
     """
-    print tags_dictionary['album']
-    print tags_dictionary['genre']
-    print tags_dictionary['year']
-    print tags_dictionary['track_number']
-    print tags_dictionary["track_title"]
-    print tags_dictionary["album_artist"]
-    print tags_dictionary["artist"]
-
+    print tags_dict['Album']
+    print tags_dict['Genre']
+    print tags_dict['Date']
+    print tags_dict['Tracknumber']
+    print tags_dict["Title"]
+    print tags_dict["Albumartist"]
+    print tags_dict["Artist"]
--- tags_retriver_ut.py
+++ tags_retriver_ut.py
@@ -5,17 +5,22 @@ import metacore
 import meta_result_parser
 from meta_utils import func_decorator
 
+
 @func_decorator
 def retriever_check(artist, song_name):
+    meta_tags_retriever.main_search(artist, song_name)
 
-    meta_tags_retriever.main(artist, song_name)
 
 @func_decorator
 def integration_check(input_file):
     result, org_dur = metacore.fingerprint_search(input_file)
     song = meta_result_parser.ResultParser(result, org_dur)
     artist, song_name = song.main()
-    meta_tags_retriever.main(artist, song_name)
+    meta_tags_retriever.main_search(artist, song_name)
+
+@func_decorator
+def lastfm_check(artist_name, track_name):
+    meta_tags_retriever.lastfm_album_art(artist_name, track_name)
 
 if __name__ == "__main__":
-    integration_check()
+    lastfm_check("The Civil Wars", "The Civil Wars")
