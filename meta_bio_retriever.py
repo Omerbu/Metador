a19aadd70c205bf8fbd2eb7ca039ef78f26ba64c
paznewman <newman.paz@gmail.com>
2016-12-11 18:08:35 +0200

    bio_retriever update
    Added search_lastfm function to the module.
    
    Added the maxchars parameter.
    
    Changed the ouput of the function from a string to a tuple - containing
    the text and the URL of the found page.
    
    Edited the docstrings.

--- meta_bio_retriever.py
+++ meta_bio_retriever.py
@@ -3,12 +3,18 @@ from bs4 import BeautifulSoup
 import urllib2
 
 
-def search_wiki(artist):
+def search_wiki(artist, maxchars=300):
     """ Retrieve artist's bio from Wikipedia.
 
-    Returns the first paragraph of a Wikipedia article as string.
-    If no article found, returns None.
+    Args:
+        artist (string): The name of the artist (or band) to be searched.
+        maxchars (int): The maximum number of characters to return.
+
+    Returns:
+        A tuple containing the retrieved text (up to maxchars) and the url of the page.
+        If no data was found, returns None.
     """
+
     url_wiki = "https://en.wikipedia.org/wiki/"
     url_search = "https://en.wikipedia.org/w/index.php?search="
     article = artist.replace(" ", "_")
@@ -32,4 +38,31 @@ def search_wiki(artist):
         cite.decompose()
     for ipa in first_paragraph.find_all("span", {"class": "IPA"}):
         ipa.parent.decompose()
-    return first_paragraph.text
+    return first_paragraph.text[:maxchars], html_content.geturl()
+
+
+def search_lastfm(artist, maxchars=300):
+    """ Retrieve artist's bio from LastFM.
+
+    Args:
+        artist (string): The name of the artist (or band) to be searched.
+        maxchars (int): The maximum number of characters to return.
+
+    Returns:
+        A tuple containing the retrieved text (up to maxchars) and the url of the page.
+        If no data was found, returns None.
+    """
+
+    url_lastfm = "http://www.last.fm/music/"
+    page = artist.replace(" ", "+") + "/+wiki"
+    try:
+        html_content = urllib2.urlopen(url_lastfm + page)
+    except urllib2.HTTPError:
+        return None
+    soup = BeautifulSoup(html_content, "html.parser")
+    wiki_content = soup.find("div", {"class": "wiki-content"})
+    try:
+        text = wiki_content.get_text(separator="\n", strip=True)
+    except AttributeError:
+        return None
+    return text[:maxchars], html_content.geturl()
